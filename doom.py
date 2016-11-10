0d0c43b22e09e5d100046ce33a70ba983c0c97b6
Eugene Kharitonov <e.kharitonov@criteo.com>
2016-11-10 20:40:14 +0300

    conv2d + checkpoints

--- doom.py
+++ doom.py
@@ -9,40 +9,62 @@ from PIL import Image
 ENV = gym.make('ppaquette/DoomBasic-v0')
 wrapper = ppaquette_gym_doom.wrappers.action_space.ToDiscrete('minimal')
 ENV = wrapper(ENV)
-X, Y = 320, 240
-#X, Y = 160, 120
+#X, Y = 320, 240
+X, Y = 160, 120
 XY = X * Y
 wrapper = ppaquette_gym_doom.wrappers.observation_space.SetResolution('%sx%s'%(X, Y))
 ENV = wrapper(ENV)
 #wrapper = ppaquette_gym_doom.wrappers.control.SetPlayingMode('human')
 #ENV = wrapper(ENV)
 
+def conv2d(x, W):
+  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
 class DoomAgent(object):
     def __init__(self):
         self.episode_states_rgb = tf.placeholder(tf.float32, [None, Y, X, 3])
-        #self.episode_state_grey = tf.image.rgb_to_grayscale(self.episode_states_rgb)
-        self.episode_state_grey = self.episode_states_rgb[:,:,:,1] - 128
-        batch_size = tf.shape(self.episode_state_grey)[0]
-        self.episode_state_as_vector = tf.reshape(self.episode_state_grey, [batch_size, XY])
+        batch_size = tf.shape(self.episode_states_rgb)[0]
+
+        #first convolution layer
+        W_conv1 = tf.Variable(tf.truncated_normal([5, 5, 3, 32], stddev = 0.1), "Wconv1")
+        b_conv1 = tf.Variable(tf.zeros([32]), "bconv1")
+
+        h_conv1 = tf.nn.relu(conv2d(self.episode_states_rgb, W_conv1) + b_conv1)
+        h_pool1 = max_pool_2x2(h_conv1)
+
+        #second convolution layer
+        W_conv2 = tf.Variable(tf.truncated_normal([5, 5, 32, 64], stddev = 0.1), "Wconv2")
+        b_conv2 = tf.Variable(tf.zeros([64]), "bconv2")
+
+        h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+        h_pool2 = max_pool_2x2(h_conv2)
+        self.h_pool2 = h_pool2
+        flat_size = 30 * 40 * 64
+        h_pool2_flat = tf.reshape(h_pool2, [-1, flat_size])
+
 
         #one fully connected layer so far
-        W1 = tf.Variable(tf.zeros([XY, 64]))
-        b1 = tf.Variable(tf.zeros([64]))
-        self.O1 = tf.nn.relu(tf.matmul(self.episode_state_as_vector, W1) + b1)
+        W1 = tf.Variable(tf.zeros([flat_size, 32]))
+        b1 = tf.Variable(tf.zeros([32]))
+        O1 = tf.nn.relu(tf.matmul(h_pool2_flat, W1) + b1)
 
         #output layer
-        W2 = tf.Variable(tf.zeros([64, 4]))
-        self.b2 = tf.Variable(tf.zeros([1, 4]))
-        self.O2 = tf.nn.softmax(tf.matmul(self.O1, W2) + self.b2)
-        #self.O2 = tf.nn.softmax(self.b2)
+        W2 = tf.Variable(tf.zeros([32, 4]))
+        b2 = tf.Variable(tf.zeros([1, 4]))
+        self.O2 = tf.nn.softmax(tf.matmul(O1, W2) + b2)
 
-        self.actions = tf.placeholder(tf.uint8, [None], 'actions') 
+        self.actions = tf.placeholder(tf.uint8, [None], 'actions')
         self.actions_one_hot = tf.one_hot(self.actions, 4, on_value = 1.0, off_value = 0.0, axis = -1)
         self.rewards = tf.placeholder(tf.float32, [None, 1], 'rewards')
 
         self.per_action_log_prob = tf.reduce_sum(self.O2 * self.actions_one_hot, reduction_indices = [1])
-        self.loss = tf.reduce_sum(self.rewards * tf.log(self.per_action_log_prob))
+        self.loss = -tf.reduce_sum(self.rewards * tf.log(self.per_action_log_prob))
         self.train_step = tf.train.AdamOptimizer(0.01).minimize(self.loss)
+
+        self.saver = tf.train.Saver([W2, b2, W1, b1, W_conv1, W_conv2, b_conv1, b_conv2])
 
         init = tf.initialize_all_variables()
         self.sess = tf.Session()
@@ -63,7 +85,7 @@ class DoomAgent(object):
         actions = np.array(self.episode_actions)
         #actions = actions.reshape((actions.shape[0], 1))
 
-        _, loss, o2, b2 = self.sess.run([self.train_step, self.loss, self.O2, self.b2], feed_dict={self.episode_states_rgb: observations, self.rewards: rewards, self.actions: actions})
+        _, loss = self.sess.run([self.train_step, self.loss], feed_dict={self.episode_states_rgb: observations, self.rewards: rewards, self.actions: actions})
         #loss, o2, t = self.sess.run([self.loss, self.O2, self.per_action_log_prob], feed_dict={self.episode_states_rgb: observations, self.rewards: rewards, self.actions: actions})
         print 'loss', loss
         #print 'o2', o2 
@@ -71,15 +93,20 @@ class DoomAgent(object):
 
     def act(self, ob):
         ob_as_batch = ob.reshape((1, Y, X, 3))
-        [o2, grey_image] = self.sess.run([self.O2, self.episode_state_grey], feed_dict={self.episode_states_rgb: ob_as_batch})
-        grey_image = grey_image.reshape((Y, X))
+        [o2] = self.sess.run([self.O2], feed_dict={self.episode_states_rgb: ob_as_batch})
+        #[pool2] = self.sess.run([self.h_pool2], feed_dict={self.episode_states_rgb: ob_as_batch})
+        #print pool2.shape
+        #exit(0)
+        #grey_image = grey_image.reshape((Y, X))
         #print ob.shape
         #print grey_image.shape
         #img = Image.fromarray(grey_image, "F")
         #img.show()
         #exit(0)
         #save('my.png')
-        #print self.O2
+        #print h2
+        #print h2.shape
+        #exit(0)
         action = npr.multinomial(1, o2[0, :]).argmax()
         self.episode_actions.append(action)
         self.episode_observations.append(ob)
@@ -110,6 +137,7 @@ def learn(episodes, max_steps_per_episode, visualisation_freq):
         agent.end_episode()
         if (episode + 1) % visualisation_freq == 0:
             ENV.monitor.close()
+            agent.saver.save(agent.sess, 'chckpoints/model', global_step=episode)
 
 if __name__ == '__main__':
     learn(50000, None, 100)
