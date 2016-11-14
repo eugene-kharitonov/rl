ef4436191bb34d5313af56e08778a2a147fd3dc0
Eugene Kharitonov <e.kharitonov@criteo.com>
2016-11-14 19:24:06 +0100

    a bit of play

--- doom.py
+++ doom.py
@@ -26,25 +26,26 @@ def max_pool_2x2(x):
 class DoomAgent(object):
     def __init__(self):
         self.episode_states_rgb = tf.placeholder(tf.float32, [None, Y, X, 3])
-        self.episode_states_crop = self.episode_states_rgb[:, 50:65, :, :]
-
+        self.episode_states_crop = self.episode_states_rgb[:, 95:115, :, 0:1]
+        crop = self.episode_states_crop
+        self.thr_crop = tf.select(crop < 200, 255*tf.ones_like(crop), tf.zeros_like(crop))
         batch_size = tf.shape(self.episode_states_rgb)[0]
 
         #first convolution layer
-        W_conv1 = tf.Variable(tf.truncated_normal([5, 5, 3,  16], stddev = 0.1), "Wconv1")
-        b_conv1 = tf.Variable(tf.zeros([16]), "bconv1")
+        W_conv1 = tf.Variable(tf.truncated_normal([5, 5, 1, 8], stddev = 0.1), "Wconv1")
+        b_conv1 = tf.Variable(tf.zeros([8]), "bconv1")
 
-        h_conv1 = tf.nn.relu(conv2d(self.episode_states_crop, W_conv1) + b_conv1)
+        h_conv1 = tf.nn.relu(conv2d(crop, W_conv1) + b_conv1)
         h_pool1 = max_pool_2x2(h_conv1)
 
         #second convolution layer
-        W_conv2 = tf.Variable(tf.truncated_normal([5, 5, 16, 8], stddev = 0.1), "Wconv2")
+        W_conv2 = tf.Variable(tf.truncated_normal([5, 5, 8, 8], stddev = 0.1), "Wconv2")
         b_conv2 = tf.Variable(tf.zeros([8]), "bconv2")
 
         h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
         h_pool2 = max_pool_2x2(h_conv2)
         self.h_pool2 = h_pool2
-        flat_size = 4 * 80 * 8
+        flat_size = 5 * 80 * 8 
         h_pool2_flat = tf.reshape(h_pool2, [-1, flat_size])
 
 
@@ -54,8 +55,8 @@ class DoomAgent(object):
         O1 = tf.nn.relu(tf.matmul(h_pool2_flat, W1) + b1)
 
         #output layer
-        W2 = tf.Variable(tf.zeros([32, 4]))
-        b2 = tf.Variable(tf.zeros([1, 4]))
+        W2 = tf.Variable(tf.zeros([32, 3]))
+        b2 = tf.Variable(tf.zeros([1, 3]))
         self.O2 = tf.nn.softmax(tf.matmul(O1, W2) + b2)
 
         #state-value layer
@@ -64,11 +65,12 @@ class DoomAgent(object):
         self.V = tf.matmul(O1, W3) + b3
 
         self.actions = tf.placeholder(tf.uint8, [None], 'actions')
-        self.actions_one_hot = tf.one_hot(self.actions, 4, on_value = 1.0, off_value = 0.0, axis = -1)
+        self.actions_one_hot = tf.one_hot(self.actions, 3, on_value = 1.0, off_value = 0.0, axis = -1)
         self.rewards = tf.placeholder(tf.float32, [None, 1], 'rewards')
 
         self.per_action_log_prob = tf.reduce_sum(self.O2 * self.actions_one_hot, reduction_indices = [1])
-        self.loss = -tf.reduce_sum((self.rewards - self.V) * tf.log(self.per_action_log_prob))
+        self.entropy = - tf.reduce_mean(tf.log(self.per_action_log_prob) * self.per_action_log_prob)
+        self.loss = -tf.reduce_sum((self.rewards - self.V) * tf.log(self.per_action_log_prob)) - 0.01 * self.entropy
         self.train_step = tf.train.AdamOptimizer().minimize(self.loss)
         self.value_loss = tf.reduce_mean(tf.square(self.rewards - self.V))
         self.v_train_step = tf.train.AdamOptimizer().minimize(self.value_loss)
@@ -94,9 +96,9 @@ class DoomAgent(object):
         observations = np.array(self.episode_observations)
         actions = np.array(self.episode_actions)
 
-        _t1, _t2, v, loss, vloss = self.sess.run([self.train_step, self.v_train_step, self.V, self.loss, self.value_loss], feed_dict={self.episode_states_rgb: observations, self.rewards: rewards, self.actions: actions})
+        _t1, _t2, v, loss, vloss, entropy = self.sess.run([self.train_step, self.v_train_step, self.V, self.loss, self.value_loss, self.entropy], feed_dict={self.episode_states_rgb: observations, self.rewards: rewards, self.actions: actions})
         #loss, o2, t = self.sess.run([self.loss, self.O2, self.per_action_log_prob], feed_dict={self.episode_states_rgb: observations, self.rewards: rewards, self.actions: actions})
-        print 'loss', loss, 'value loss', vloss, v.shape
+        print 'loss', loss, 'value loss', vloss, v.shape, entropy
         #print 'o2', o2 
         #print 'b2', b2
 
@@ -104,13 +106,14 @@ class DoomAgent(object):
         ob_as_batch = ob.reshape((1, Y, X, 3))
         [o2] = self.sess.run([self.O2], feed_dict={self.episode_states_rgb: ob_as_batch})
         #[h2] = self.sess.run([self.h_pool2], feed_dict={self.episode_states_rgb: ob_as_batch})
-        #[crop] = self.sess.run([self.episode_states_crop], feed_dict={self.episode_states_rgb: ob_as_batch})
-        #print h2.shape
+        #[crop, pool] = self.sess.run([self.thr_crop, self.h_pool2], feed_dict={self.episode_states_rgb: ob_as_batch})
+        #print pool.shape
         #exit(0)
         #grey_image = grey_image.reshape((Y, X))
         #print grey_image.shape
-        #crop = crop[0, 50:65, :, 0]
-        #print crop.shape
+        #crop = crop[0, :, :, 0]
+        #crop[crop < 200] = 0
+        #print crop.shape, pool.shape
         #img = Image.fromarray(crop, "F")
         #img.show()
         #exit(0)
@@ -118,11 +121,12 @@ class DoomAgent(object):
         #print h2
         #print h2.shape
         #exit(0)
-        action = npr.multinomial(1, o2[0, :]).argmax()
-        self.episode_actions.append(action)
-        self.episode_observations.append(ob)
+        action = npr.multinomial(1, o2[0, :]).argmax() 
 
-        return action
+        return action + 1 #no NOOP
+    def record_action(self, action, ob):
+        self.episode_actions.append(action - 1)
+        self.episode_observations.append(ob)
 
 def learn(episodes, max_steps_per_episode, visualisation_freq):
     outdir = './output/'
@@ -136,14 +140,27 @@ def learn(episodes, max_steps_per_episode, visualisation_freq):
         rewards = []
         agent.start_episode()
         j = 0
-        while not done and j < 3 * 35: #3 sec
-            j += 1
-            action = agent.act(ob)
-            new_ob, reward, done, info = ENV.step(action)
-            agent.feedback(reward)
+        last_action = 0
+        while not done and j < 5 * 35: #3 sec
+            if j % 5 == 0:
+                action = agent.act(ob)
+                last_action = action
+                agent.record_action(action, ob)
+                new_ob, reward, done, info = ENV.step(action)
+                agent.feedback(reward)
+                if done:
+                    break
+            else:
+                action = last_action
+                new_ob, reward, done, info = ENV.step(action)
+                if done:
+                    agent.record_action(action, ob)
+                    agent.feedback(reward)
+                    break
+
             ob = new_ob
-            if done:
-                break
+            j += 1
+
         print info['TOTAL_REWARD']
         agent.end_episode()
         if (episode + 1) % visualisation_freq == 0:
